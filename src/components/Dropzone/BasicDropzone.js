af9d495033269dcd896ad7b475f41f292cc5c812
Jason Vance <jbvance@gmail.com>
2019-07-01 11:58:01 -0500

    Refactor Dropzone component
    Refactor Dropzone component

--- src/components/Dropzone/BasicDropzone.js
+++ src/components/Dropzone/BasicDropzone.js
@@ -1,110 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import Dropzone from "react-dropzone";
-import moment from "moment";
-
 import "./Dropzone.css";
-import CryptoJS from "crypto-js";
 
-import Button from "react-bootstrap/Button";
+const Basic = props => {
+  const onDrop = files => {
+    props.onFileChange(files);
+  };
 
-class Basic extends Component {
-  constructor() {
-    super();
+  //const maxSize = 1048576;
+  const maxSize = 52428800;
+  //console.log(this.state.files)
 
-    this.state = {
-      files: []
-    };
-
-    this.onDrop = this.onDrop.bind(this);
-  }
-
-  onDrop(files) {
-    this.setState({
-      files: this.state.files.concat(files)
-    });
-  }
-
-  setupReader(file) {
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const file_result = reader.result;
-      //alert(file_result);
-      const file_wordArr = CryptoJS.lib.WordArray.create(file_result); //convert blob to WordArray , see https://code.google.com/p/crypto-js/issues/detail?id=67
-      const sha256_hash = CryptoJS.SHA256(file_wordArr); //calculate SHA1 hash
-      alert("Calculated SHA256:" + sha256_hash.toString()); //output result
-    };
-    reader.readAsText(file, "UTF-8");
-  }
-
-  CreateHashes() {
-    const { files } = this.state;
-    files.forEach(file => {
-      this.setupReader(file);
-    });
-  }
-
-  render() {
-    //const maxSize = 1048576;
-    const maxSize = 52428800;
-    //console.log(this.state.files)
-    const files = this.state.files.map(file => (
-      <li key={file.name}>
-        {file.name} - {file.type} -{" "}
-        {moment(file.lastModified).format("MM-DD-YYYY HH:mm:ss")}
-      </li>
-    ));
-
-    return (
-      <div>
-        <div className="text-center mt-5">
-          <Dropzone
-            onDrop={this.onDrop}
-            minSize={0}
-            maxSize={maxSize}
-            multiple={true}
-          >
-            {({
-              getRootProps,
-              getInputProps,
-              isDragActive,
-              isDragReject,
-              rejectedFiles
-            }) => {
-              const isFileTooLarge =
-                rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
-              return (
-                <div
-                  {...getRootProps()}
-                  className={`Dropzone ${isDragActive ? "Highlight" : ""}`}
-                >
-                  <input {...getInputProps()} />
-                  {!isDragActive && "Click here or drop a file to upload!"}
-                  {isDragActive && !isDragReject && "Drop it like it's hot!"}
-                  {isDragReject && "File type not accepted, sorry!"}
-                  {isFileTooLarge && (
-                    <div className="text-danger mt-2">File is too large.</div>
-                  )}
-                </div>
-              );
-            }}
-          </Dropzone>
-        </div>
-        <div>
-          <h4>Files</h4>
-          {files}
-        </div>
-        <div>
-          <Button
-            disabled={this.state.files.length < 1}
-            variant="primary"
-            onClick={e => this.CreateHashes()}
-          >
-            Upload File Hashes
-          </Button>
-        </div>
+  return (
+    <div>
+      <div className="text-center mt-5">
+        <Dropzone onDrop={onDrop} minSize={0} maxSize={maxSize} multiple={true}>
+          {({
+            getRootProps,
+            getInputProps,
+            isDragActive,
+            isDragReject,
+            rejectedFiles
+          }) => {
+            const isFileTooLarge =
+              rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+            return (
+              <div
+                {...getRootProps()}
+                className={`Dropzone ${isDragActive ? "Highlight" : ""}`}
+              >
+                <input {...getInputProps()} />
+                {!isDragActive && "Click here or drop a file to upload!"}
+                {isDragActive && !isDragReject && "Drop it like it's hot!"}
+                {isDragReject && "File type not accepted, sorry!"}
+                {isFileTooLarge && (
+                  <div className="text-danger mt-2">File is too large.</div>
+                )}
+              </div>
+            );
+          }}
+        </Dropzone>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Basic;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import CryptoJS from "crypto-js";
 import Web3 from "web3";
 import { DOCPROOF_ADDRESS, DOCPROOF_ABI } from "./contracts/config";
+import moment from "moment";
 //import Dropzone from "./components/Dropzone/Dropzone";
 
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
 import Spinner from "react-bootstrap/Spinner";
 
 import Dropzone from "./components/Dropzone/BasicDropzone";
-
-import Button from "react-bootstrap/Button";
 
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -20,16 +21,18 @@ class App extends React.Component {
     this.state = {
       loading: false,
       docProof: null,
-      account: null
+      account: null,
+      files: []
     };
 
     this.loadBlockchainData = this.loadBlockchainData.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
   }
 
   async componentDidMount() {
     try {
       this.setState({ loading: true });
-      await this.loadBlockchainData();
+      //await this.loadBlockchainData();
       this.setState({ loading: false });
     } catch (err) {
       console.log(err);
@@ -51,8 +54,63 @@ class App extends React.Component {
     console.log("currentFileIndex", currentFileIndex);
   }
 
+  setupReader(file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const file_result = reader.result;
+      //alert(file_result);
+      const file_wordArr = CryptoJS.lib.WordArray.create(file_result); //convert blob to WordArray , see https://code.google.com/p/crypto-js/issues/detail?id=67
+      const sha256_hash = CryptoJS.SHA256(file_wordArr); //calculate SHA1 hash
+      alert("Calculated SHA256:" + sha256_hash.toString()); //output result
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  CreateHashes() {
+    const { files } = this.state;
+    files.forEach(file => {
+      this.setupReader(file);
+    });
+  }
+
+  handleFileChange(files) {
+    console.log("Files changed");
+    this.setState(
+      {
+        files: this.state.files.concat(files)
+      },
+      () => console.log(this.state.files)
+    );
+  }
+
+  renderFileList() {
+    return this.state.files.map(file => (
+      <li key={file.name}>
+        <Alert
+          variant="success"
+          dismissible={true}
+          onClose={e => this.removeFile(file.name)}
+        >
+          File Name: {file.name} <br />
+          File Type: {file.type}
+          <br />
+          Last Modified:{" "}
+          {moment(file.lastModified).format("MM-DD-YYYY HH:mm:ss")}
+        </Alert>
+      </li>
+    ));
+  }
+
+  removeFile(fileName) {
+    const updatedFiles = [...this.state.files].filter(
+      file => file.name !== fileName
+    );
+    this.setState({ files: updatedFiles });
+  }
+
   render() {
     //console.log("HASH", CryptoJS.SHA256("testing"));
+
     return (
       <div>
         <nav className="navbar navbar-dark  bg-dark flex-md-nowrap p-0 mb-5 shadow">
@@ -80,8 +138,21 @@ class App extends React.Component {
               <span>...Loading</span>
             </div>
           ) : (
-            <Dropzone />
+            <Dropzone onFileChange={this.handleFileChange} />
           )}
+          <div className="file-list">
+            <h4>Files</h4>
+            {this.renderFileList()}
+          </div>
+          <div>
+            <Button
+              disabled={this.state.files.length < 1}
+              variant="primary"
+              onClick={e => this.CreateHashes()}
+            >
+              Upload File Hashes
+            </Button>
+          </div>
         </div>
       </div>
     );
